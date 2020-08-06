39ddebb3c4c5a1bda2cc68b2ef8ccf547baefdb6
Prathyusha Davuluru <dprathyusha252@gmail.com>
2020-08-05 21:10:10 -0400

    Added supporting schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewsLetterComponent } from './newsletter.component';
@@ -15,6 +15,7 @@ import {NewsLetterService} from './newsletter.service';
     BrowserModule
   ],
   providers: [NewsLetterService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
