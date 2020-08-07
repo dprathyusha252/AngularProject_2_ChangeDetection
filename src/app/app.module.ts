5f321557fed190529c69426ceccc09d96d625095
Prathyusha Davuluru <dprathyusha252@gmail.com>
2020-08-06 21:33:49 -0400

    Removed unused declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NewsLetterComponent } from './newsletter.component';
 import {NewsLetterService} from './newsletter.service';
